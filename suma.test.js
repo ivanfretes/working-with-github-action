95bd04bb5e135eb03d387d4938c83bab3875be63
Ivan <vanfretes@gmail.com>
2023-03-24 14:09:47 -0300

    fix: test

--- suma.test.js
+++ suma.test.js
@@ -1,5 +1,5 @@
 const operaciones = require('./suma')
 
 test('deberia retornar el resultado de la suma', () => {
-   expect(operaciones.sumar(5,6)).toBe(12);
+   expect(operaciones.sumar(5,6)).toBe(11);
 });
